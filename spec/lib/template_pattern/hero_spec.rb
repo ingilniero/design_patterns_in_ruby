13147a4ad3b24607ea8aafa1c1e1f2340604c676
Gilberto Villa <ingilniero@gmail.com>
2014-01-04 16:28:10 -0600

    Add new requirement for hero abilities

--- spec/lib/template_pattern/hero_spec.rb
+++ spec/lib/template_pattern/hero_spec.rb
@@ -19,5 +19,10 @@ describe Hero do
     it 'has a default damage rating of "15"' do
       expect(subject.damage).to eq 15
     end
+
+    it 'has special ability Strike' do
+      expect(subject.abilities).to include(:strike)
+    end
   end
+
 end
