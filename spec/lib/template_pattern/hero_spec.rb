3c06556517b5e3fc57a981a633d48f89b14be61b
Gilberto Villa <ingilniero@gmail.com>
2014-01-04 16:22:56 -0600

    Add Warrior context

--- spec/lib/template_pattern/hero_spec.rb
+++ spec/lib/template_pattern/hero_spec.rb
@@ -1,11 +1,19 @@
 require 'spec_helper'
 
 describe Hero do
- it 'has default damage rating of "10"' do
-   expect(subject.damage).to eq 10
- end
+  context 'Default Hero' do
+    it 'has default damage rating of "10"' do
+      expect(subject.damage).to eq 10
+    end
 
- it 'can attack' do
-   expect(subject.attack).to eq 'Attacked dealing 10 damage'
- end
+    it 'can attack' do
+      expect(subject.attack).to eq 'Attacked dealing 10 damage'
+    end
+  end
+
+  context 'Warrior' do
+    it 'has a default damage rating of "15"' do
+      expect(subject.damage).to eq 15
+    end
+  end
 end
