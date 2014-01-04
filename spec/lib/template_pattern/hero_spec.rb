7d830d13c06908e45a315240b138df41b16cb044
Gilberto Villa <ingilniero@gmail.com>
2014-01-04 16:20:36 -0600

    Template Pattern

---
+++ spec/lib/template_pattern/hero_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Hero do
+ it 'has default damage rating of "10"' do
+   expect(subject.damage).to eq 10
+ end
+
+ it 'can attack' do
+   expect(subject.attack).to eq 'Attacked dealing 10 damage'
+ end
+end
