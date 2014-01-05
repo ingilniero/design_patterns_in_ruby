be837152e4e0764b9e35990746b4cc201a7158c7
Gilberto Villa <ingilniero@gmail.com>
2014-01-04 20:29:51 -0600

    Observer pattern

---
+++ spec/lib/observer_pattern/hero_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe Observer::Hero do
+
+  let(:tile) { double('tile') }
+
+  describe '#cursed?' do
+    context 'when is not cursed' do
+      it { expect(subject.cursed?).to be_false }
+    end
+
+    context 'when is cursed' do
+      before { subject.discover(tile) }
+      it { expect(subject.cursed?).to be_true }
+    end
+  end
+
+  describe '#discover' do
+    it 'changes cursed state to true' do
+      expect {subject.discover }.to change(subject.cursed?).from(false).to(true)
+    end
+  end
+
+end
