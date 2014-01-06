bb5e76a6bfffacdb39dac1c3778711c1e25f602b
Gilberto Villa <ingilniero@gmail.com>
2014-01-06 14:33:50 -0600

    Party can create several heroes

--- lib/factory_pattern/party.rb
+++ lib/factory_pattern/party.rb
@@ -2,8 +2,14 @@ module Factory
   class Party
     attr_reader :members
 
-    def initialize
+    def initialize(attrs = {})
       @members = []
+      number = attrs.fetch(:number,0)
+      number.times { members << create }
+    end
+
+    def create
+      Hero.new
     end
 
   end
--- spec/lib/factory_pattern/party_spec.rb
+++ spec/lib/factory_pattern/party_spec.rb
@@ -1,7 +1,22 @@
 require 'spec_helper'
 
 describe Factory::Party do
+
+  let(:factory) { Factory::Party.new number: 3  }
+
   it 'has members' do
     expect(subject.members).to eq []
+  end
+
+  it 'create several heros' do
+    expect(factory.members.count{ |member| member.class == Hero }).to eq 3
+  end
+
+  describe '#create' do
+    let(:hero) { subject.create }
+
+    it 'creates a Warrior' do
+      expect(hero).to be_instance_of Hero
+    end
   end
 end
