2d7743a66939742bd8bba3dd4d87798ae3224d61
Gilberto Villa <ingilniero@gmail.com>
2014-01-07 20:51:03 -0600

    Decorators can be layered

--- spec/lib/decorator_pattern/item_spec.rb
+++ spec/lib/decorator_pattern/item_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Decorator::CommonItem do
-  let(:full_item) { Decorator::CommonItem.new true, true }
+  let(:magic_item) { Decorator::MagicDecorator.new subject }
 
   it 'has price' do
     expect(subject.price).to eq 10
@@ -16,12 +16,24 @@ describe Decorator::CommonItem do
   end
 
   describe 'full item' do
+    let(:full_item) { Decorator::MasterpieceDecorator.new magic_item }
+
     it 'is six times more expensive than common item' do
       expect(full_item.price).to eq subject.price * 6
     end
 
-    it 'has another description' do
-      expect(full_item.description).to eq subject.description + "Magic.Masterpiece."
+    context 'when is mostly a master piece' do
+      it 'has masterpiece description' do
+        expect(full_item.description).to eq subject.description + "Magic.Masterpiece."
+      end
     end
+
+    context 'when is mostly magical' do
+      let(:full_item) { Decorator::MagicDecorator.new Decorator::MasterpieceDecorator.new subject }
+      it 'has a magical description' do
+        expect(full_item.description).to eq subject.description + "Masterpiece.Magic."
+      end
+    end
+
   end
 end
