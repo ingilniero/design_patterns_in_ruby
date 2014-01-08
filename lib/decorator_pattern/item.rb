01658b33b20d12c31be6b10b3f7a21f9bacbb93b
Gilberto Villa <ingilniero@gmail.com>
2014-01-07 20:06:25 -0600

    Introduce magic Item requirement

--- lib/decorator_pattern/item.rb
+++ lib/decorator_pattern/item.rb
@@ -2,9 +2,14 @@ module Decorator
   class CommonItem
     attr_reader :price, :description
 
-    def initialize
+    def initialize(is_magic = false)
       @price = 10
       @description = 'Item.'
+
+      if is_magic
+        @price *= 3
+        @description += 'Magic.'
+      end
     end
 
     def use
--- spec/lib/decorator_pattern/item_spec.rb
+++ spec/lib/decorator_pattern/item_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe Decorator::CommonItem do
+  let(:magic_item) { Decorator::CommonItem.new true }
+
   it 'has price' do
     expect(subject.price).to eq 10
   end
@@ -11,5 +13,15 @@ describe Decorator::CommonItem do
 
   it 'can be used' do
     expect(subject).to respond_to(:use)
+  end
+
+  describe 'magic items' do
+    it 'is three times more expensive than common item' do
+      expect(magic_item.price).to eq subject.price * 3
+    end
+
+    it 'has another description' do
+      expect(magic_item.description).to eq subject.description + "Magic."
+    end
   end
 end
