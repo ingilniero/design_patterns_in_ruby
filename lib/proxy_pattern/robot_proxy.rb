929e1cbfbf84dfa2c92400a75bfdb7ff80b417eb
Gilberto Villa <ingilniero@gmail.com>
2014-01-07 21:24:26 -0600

    Check for permissions before execute robot commands

--- lib/proxy_pattern/robot_proxy.rb
+++ lib/proxy_pattern/robot_proxy.rb
@@ -1,11 +1,21 @@
 module Proxy
   class RobotProxy
-    def initialize(robot)
+    def initialize(robot, operator)
       @robot = robot
+      @operator = operator
     end
 
     def method_missing(name, *args)
       @robot.send(name, *args)
+    end
+
+    def execute
+      check_access
+      @robot.execute
+    end
+
+    def check_access
+      raise 'Robot Apocalypsis' unless @operator.vehicles.include? :robot
     end
   end
 end
--- spec/lib/proxy_pattern/robot_proxy_spec.rb
+++ spec/lib/proxy_pattern/robot_proxy_spec.rb
@@ -3,22 +3,31 @@ require 'spec_helper'
 describe Proxy::RobotProxy do
 
   let(:robot) { double('robot', add: [], queue: [], execute: true) }
+  let(:operator) { double('operator', vehicles: [ :robot ]) }
 
-  subject { Proxy::RobotProxy.new robot }
+  let(:robot_proxy) { Proxy::RobotProxy.new robot, operator }
 
   describe '#add' do
     before { robot.should_receive(:add) }
 
     it 'delegates all method to the robot' do
-      subject.add
+      robot_proxy.add
     end
   end
 
-  describe '#add' do
-    before { robot.should_receive(:execute) }
+  describe '#execute' do
+    context 'when operator has permission' do
+      before { robot.should_receive(:execute) }
 
-    it 'delegates all method to the robot' do
-      subject.execute
+      it 'delegates all method to the robot' do
+        robot_proxy.execute
+      end
+    end
+
+    context 'when operator is unauthorized' do
+      before { operator.stub(:vehicles).and_return(:batimovil) }
+
+      it { expect{ robot_proxy.execute }.to raise_exception }
     end
   end
 end
